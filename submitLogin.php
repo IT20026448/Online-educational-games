4918d751fab78b2ea1c456555bee4ebfa2b2daf9
IT20026448 <73211515+IT20026448@users.noreply.github.com>
2020-10-21 18:49:17 +0530

    Update submitLogin.php

--- submitLogin.php
+++ submitLogin.php
@@ -5,37 +5,17 @@
 <?php
 	$email = $_POST["email"];
 	$pass = $_POST["password"];
-	$emailInDB ="SELECT Email FROM login WHERE Email = $email";
-	$passInDB = "SELECT Password FROM login WHERE Password = $password";
-	$name = "SELECT Name from login WHERE Email = $email";
 	
-	$sql = "select * from item";
+	$sql = "select Email, Password from login";
 	$result = $conn->query($sql);
 						
-	if($result->num_rows>0)
+	while($row = $result->fetch_assoc())
 	{
-		while($row = $result->fetch_assoc())
+		if($email == $row["Email"] && $pass == $row["Password"])
 		{
-			$row["Email", "Password", "Name"];
-			
-			if($email == $emailInDB && $pass == $passInDB)
-			{
-				echo "<script> alert('Login successful') </script>";
-				echo "<script> Welcome $name </script>";
-			}
-			else
-			{
-				echo "<script> alert('Invalid password!!! Please re-enter password') </script>";
-			}
+			header("Location:useraccount.html");
 		}
 	}
-	else
-	{
-		echo "0 results";
-	}
-						
 	
-		
-	
-	mysqli_close($conn);
+	$conn->close();
 ?>
